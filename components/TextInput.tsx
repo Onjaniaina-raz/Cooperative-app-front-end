3feac718d8f837e18ec48e31b4bd7a2aa55821c2
Onja <rajohnarson01@gmail.com>
2025-10-16 04:21:17 -0700

    Input component update

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -4,7 +4,7 @@ const TextInput = () => {
   return (
     <label className='relative'>
       <input type='text' className='px-6 h-[50px] w-full bg-white border outline-none focus:border-coop-5 transition duration-200 rounded-lg'/>
-      <span className='text-opacity-80 absolute left-0 top-0 mx-6 px-2 transition duration-200 input-text'>Input</span>
+      <span className='text-opacity-80 absolute left-0 top-0 mx-6 transition duration-200 input-text'>Input</span>
     </label>
   )
 }
